65afd42710ea1c83fa52d2d01a9f57d9e304d1c1
ramon05 <ramoncastillo051991@gmail.com>
2018-05-09 01:52:49 -0400

    agregue formulario de recuperar contraseña

---
+++ pagina3.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+	<title>Usuario</title>.
+	<link rel="stylesheet" type="text/css" href="CSS/pagina3.css">
+	<link rel="stylesheet" type="text/css" href="fonts/style.css">
+</head>
+<body>
+     <form>
+         <center><img src="img/favicon.ico" id="imgRecuperar"></center>
+         <center><label id="lbl-recuperar">Recuperar Contraseña</label></center>
+
+         <div class="imgUsuarioRecup">
+         	<input type="text" name="usurio" id="usuario" placeholder="nombre usuario"></br></br>
+	        <i class="icon-user"></i>
+         </div>
+
+	     <center><input type="button" id="btnRecuperar" name="Iniciar" value="Iniciar Sesión"></center>
+
+	     <label id="volver">Regresa al usuario <a href="pagina1.php">Volver al Usuario</a></label>
+     </form>    
+</body>
+</html>
